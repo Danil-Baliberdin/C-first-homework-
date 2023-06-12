fd080baa86b6a9adab9683230de565d58bd5d75a
Danil <okiidokii22@gmail.com>
2023-06-13 00:43:25 +0500

    2 and 4 tasks is done

--- C#firstHomework/SecondTask/Program.cs
+++ C#firstHomework/SecondTask/Program.cs
@@ -1,12 +1,18 @@
 ﻿int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());;
+int b = Convert.ToInt32(Console.ReadLine());
 if(a>b){
+Console.Write("max = ");
 Console.WriteLine(a);
-}
-else if(b<a){ 
+Console.Write("min = ");
 Console.WriteLine(b);
 }
-else 
+if(b>a){ 
+Console.Write("max = ");
+Console.WriteLine(b);
+Console.Write("min = ");
+Console.WriteLine(a);
+}
+if (b == a) 
 {
 Console.WriteLine("a = b");
 }
--- C#firstHomework/fourthTask/Program.cs
+++ C#firstHomework/fourthTask/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int sMax;
+if (b>c) sMax= b;
+else sMax = c;
+if (sMax>max) {
+    Console.WriteLine(sMax);
+}
+else Console.WriteLine(max);
