71b6cc3e51e7ddc1cb42294d0bd84094150086a5
Danil <okiidokii22@gmail.com>
2023-06-15 14:53:21 +0500

    10 task is done

---
+++ C#SecondHomework/task10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number % 100 / 10;
+    Console.WriteLine(secondDigit);
+}
+else Console.WriteLine("wrong number");
